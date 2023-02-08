451777bb4f883996a28026628aaa7592fba30e4f
David Cao <david@cao.sh>
2023-02-07 19:39:56 -0800

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                marquee: 'marquee 60s linear infinite',
+            },
+            keyframes: {
+                marquee: {
+                    '0%': { transform: 'translateX(0%)' },
+                    '100%': { transform: 'translateX(-50%)' }
+                },
+            },
+        }
+
     },
     plugins: [],
 }
