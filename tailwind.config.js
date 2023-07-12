c0a3712dcf52e6e0c232a116e13498c8d443e67e
David Cao <david@cao.sh>
2023-07-12 15:37:40 -0700

    new design v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,25 @@ module.exports = {
                     '100%': { transform: 'translateX(-50%)' }
                 },
             },
-        }
-
+            colors: {
+                brown: {
+                    50: '#fdf8f6',
+                    100: '#f2e8e5',
+                    200: '#eaddd7',
+                    300: '#e0cec7',
+                    400: '#d2bab0',
+                    500: '#bfa094',
+                    600: '#a18072',
+                    700: '#977669',
+                    800: '#846358',
+                    900: '#43302b',
+                },
+                cream: "#faf6ea",
+            },
+        },
+        fontFamily: {
+            'space': ['"Space Grotesk"'],
+        },
     },
     plugins: [],
 }
